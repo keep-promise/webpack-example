1eb4b47426c3c61df2b5af50b79a0954223cdc89
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-08-18 15:05:25 +0800

    Update webpack.config.js

--- example6/webpack.config.js
+++ example6/webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './main.js',
+  entry: './index.js',
   output: {
     filename: 'bundle.js'
   },
